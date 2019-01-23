d2064f5bc95ec25cf12a39649da1ee824cf4b856
Abhisek Paul <abhisekpaul@gmail.com>
2019-01-23 10:03:55 +0000

    fixed number min max length

--- test/multiple-rules.js
+++ test/multiple-rules.js
@@ -46,6 +46,57 @@ describe('Multiple rules test', function () {
 
         });
 
+        it('should return true if the number satisfies min and max', async () => {
+
+            let v = new Validator({ rate: '2.29' }, { rate: 'required|numeric|min:1|max:98' });
+
+            let matched = await v.check();
+
+            assert.equal(matched, true);
+
+        });
+
+        it('should return true if the integer satisfies min and max', async () => {
+
+            let v = new Validator({ rate: '2' }, { rate: 'required|numeric|min:1|max:98' });
+
+            let matched = await v.check();
+
+            assert.equal(matched, true);
+
+        });
+
+        it('should return false if the number does not satisfies min and max', async () => {
+
+            let v = new Validator({ rate: '2.29' }, { rate: 'required|numeric|min:5|max:98' });
+
+            let matched = await v.check();
+
+            assert.equal(matched, false);
+
+            let v2 = new Validator({ rate: '99.9' }, { rate: 'required|numeric|min:5|max:98' });
+
+            let matched2 = await v2.check();
+
+            assert.equal(matched2, false);
+
+        });
+
+        it('should return true if the integer satisfies min and max', async () => {
+
+            let v = new Validator({ rate: 2 }, { rate: 'required|numeric|min:5|max:98' });
+
+            let matched = await v.check();
+
+            assert.equal(matched, false);
+
+            let v2 = new Validator({ rate: 99 }, { rate: 'required|numeric|min:5|max:98' });
+
+            let matched2 = await v2.check();
+
+            assert.equal(matched2, false);
+
+        });
     });
 
 });
